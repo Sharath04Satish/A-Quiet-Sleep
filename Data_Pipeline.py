aa977bd5d7dddd7453a8cd51cf9fb50c4b245780
sharath04satish <sharaths1998@gmail.com>
2024-04-20 18:48:16 -0600

    Completed data pipeline and ml model in pandas

--- Data_Pipeline.py
+++ Data_Pipeline.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
+from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import time
+
+start_time = time.time()
 
 fitbit_dataset1 = pd.read_json("./data/fitbit_sleep_data_1.json")
 fitbit_dataset2 = pd.read_json("data/fitbit_sleep_data_2.json")
@@ -43,7 +51,7 @@ for index, row in selected_data.iterrows():
         flattened_data.append(flattened_row)
 
 fitbit_df = pd.DataFrame(flattened_data)
-fitbit_df['source'] = 'fitbit'
+fitbit_df["source"] = "fitbit"
 fitbit_df.dropna(inplace=True)
 print("Fitbit data is cleaned. Shape: ", fitbit_df.shape)
 
@@ -144,12 +152,16 @@ applewatch_df["time_minutes"] = applewatch_df["Start Time"].dt.minute
 applewatch_df["time_seconds"] = applewatch_df["Start Time"].dt.second
 
 applewatch_df.drop(columns={"End Time", "Start Time", "Heart Rate"}, inplace=True)
-applewatch_df['group'] = ((applewatch_df['time_hours'] >= 15) & (applewatch_df['time_hours'] < 24)).astype(int)
-applewatch_df['date_of_sleep'] = np.where(applewatch_df['group'] == 1, 
-                                          applewatch_df['date_of_sleep'] + pd.Timedelta(days=1),
-                                          applewatch_df['date_of_sleep'])
-applewatch_df.drop('group', axis = 1, inplace=True)
-applewatch_df['source'] = 'applewatch'
+applewatch_df["group"] = (
+    (applewatch_df["time_hours"] >= 15) & (applewatch_df["time_hours"] < 24)
+).astype(int)
+applewatch_df["date_of_sleep"] = np.where(
+    applewatch_df["group"] == 1,
+    applewatch_df["date_of_sleep"] + pd.Timedelta(days=1),
+    applewatch_df["date_of_sleep"],
+)
+applewatch_df.drop("group", axis=1, inplace=True)
+applewatch_df["source"] = "applewatch"
 
 
 print("Apple Watch data is cleaned. Shape: ", applewatch_df.shape)
@@ -157,29 +169,298 @@ final_dataset = pd.concat([fitbit_df, applewatch_df])
 
 final_dataset["category"] = final_dataset["category"].replace({"wake": "awake"})
 
-final_dataset["weekday"] = pd.to_datetime(final_dataset["date_of_sleep"]).dt.strftime("%A")
+final_dataset["weekday"] = pd.to_datetime(final_dataset["date_of_sleep"]).dt.strftime(
+    "%A"
+)
 
 final_dataset["date_of_sleep"] = pd.to_datetime(final_dataset["date_of_sleep"])
+
 
 def time_to_sleep_stages(group, sleep_stages):
     time_to_stages = {}
 
     for stage in sleep_stages:
-        stage_rows = group[group['category'] == stage]
+        stage_rows = group[group["category"] == stage]
         if not stage_rows.empty:
             stage_index = stage_rows.index[0]
-            seconds_before_stage = group.loc[:stage_index-1, 'seconds_count'].sum()
+            seconds_before_stage = group.loc[: stage_index - 1, "seconds_count"].sum()
             time_to_stages[stage] = seconds_before_stage
         else:
             time_to_stages[stage] = None
 
     return pd.Series(time_to_stages)
 
-time_to_sleep_stages_df = final_dataset.groupby(['date_of_sleep']).apply(time_to_sleep_stages, sleep_stages=['deep', 'rem'])
 
-final_dataset = final_dataset.merge(time_to_sleep_stages_df, left_on='date_of_sleep', right_index=True)
+time_to_sleep_stages_df = final_dataset.groupby(["date_of_sleep"]).apply(
+    time_to_sleep_stages, sleep_stages=["deep", "rem"]
+)
 
-final_dataset.rename(columns={'deep': 'time_to_deep_seconds', 'rem': 'time_to_rem_seconds'}, inplace=True)
-# final_dataset = final_dataset.drop(columns='date_of_sleep')
+stage1_dataset = final_dataset.merge(
+    time_to_sleep_stages_df, left_on="date_of_sleep", right_index=True
+)
+stage1_dataset.rename(
+    columns={"deep": "time_to_deep_seconds", "rem": "time_to_rem_seconds"}, inplace=True
+)
 
-final_dataset.to_csv("./data/final_dataset.csv", index=False)
+# final_dataset.to_csv("./data/final_dataset.csv", index=False)
+
+stage1_dataset["date_of_sleep"] = pd.to_datetime(stage1_dataset["date_of_sleep"])
+data_f = stage1_dataset.groupby("date_of_sleep").first().reset_index()
+data_l = stage1_dataset.groupby("date_of_sleep").last().reset_index()
+
+weekly_f_sleep = []
+weekly_l_sleep = []
+for i in range(0, len(data_f), 7):
+    chunk = data_f.iloc[i : i + 7]
+    weekly_f_sleep.append(chunk)
+
+for i in range(0, len(data_l), 7):
+    chunk = data_l.iloc[i : i + 7]
+    weekly_l_sleep.append(chunk)
+
+stage_seconds = [
+    [
+        cat["awake_seconds"].sum(),
+        cat["light_seconds"].sum(),
+        cat["deep_seconds"].sum(),
+        cat["rem_seconds"].sum(),
+    ]
+    for cat in weekly_f_sleep
+]
+
+stage_proportions = [week / sum(week) for week in stage_seconds]
+
+st_start_time = []
+st_end_time = []
+
+for week in weekly_f_sleep:
+    start_times = list()
+    for _, row in week.iterrows():
+        if row["time_hours"] >= 17 and row["time_hours"] <= 23:
+            time_obj = datetime(
+                2024, 12, 1, row["time_hours"], row["time_minutes"], row["time_seconds"]
+            ).timestamp()
+        else:
+            time_obj = datetime(
+                2024, 12, 2, row["time_hours"], row["time_minutes"], row["time_seconds"]
+            ).timestamp()
+
+        start_times.append(time_obj)
+
+    min_time = str(datetime.fromtimestamp(min(start_times)).time())
+    max_time = str(datetime.fromtimestamp(max(start_times)).time())
+    avg_time = str(datetime.fromtimestamp(sum(start_times) // len(start_times)).time())
+    time_range = str(datetime.fromtimestamp(max(start_times) - min(start_times)).time())
+
+    st_start_time.append((min_time, max_time, avg_time, time_range))
+
+
+for week in weekly_l_sleep:
+    end_times = list()
+    for _, row in week.iterrows():
+        if row["time_hours"] >= 17 and row["time_hours"] <= 23:
+            time_obj = datetime(
+                2024, 12, 1, row["time_hours"], row["time_minutes"], row["time_seconds"]
+            ).timestamp()
+        else:
+            time_obj = datetime(
+                2024, 12, 2, row["time_hours"], row["time_minutes"], row["time_seconds"]
+            ).timestamp()
+
+        end_times.append(time_obj)
+
+    min_time = str(datetime.fromtimestamp(min(end_times)).time())
+    max_time = str(datetime.fromtimestamp(max(end_times)).time())
+    avg_time = str(datetime.fromtimestamp(sum(end_times) // len(end_times)).time())
+    time_range = str(datetime.fromtimestamp(max(end_times) - min(end_times)).time())
+
+    st_end_time.append((min_time, max_time, avg_time, time_range))
+
+time_to_sound_sleep = []
+
+for week in weekly_f_sleep:
+    time_to_sound_sleep.append(
+        (
+            round(week.loc[:, "time_to_deep_seconds"].mean(), 2),
+            round(week.loc[:, "time_to_rem_seconds"].mean(), 2),
+        )
+    )
+
+prop_sound_sleep = []
+
+for week in weekly_f_sleep:
+    deep, rem = 0, 0
+
+    for _, row in week.iterrows():
+        deep += (
+            row["time_to_deep_seconds"]
+            / (row["asleep_seconds"] + row["awake_seconds"])
+            * 100
+        )
+        rem += (
+            row["time_to_rem_seconds"]
+            / (row["asleep_seconds"] + row["awake_seconds"])
+            * 100
+        )
+
+    deep /= week.shape[0]
+    rem /= week.shape[0]
+
+    prop_sound_sleep.append((round(deep, 2), round(rem, 2)))
+
+final_insights = {
+    "week_start": [
+        str(weekly_f_sleep[index].iloc[0, 0].date())
+        for index in range(len(weekly_f_sleep))
+    ],
+    "week_end": [
+        str(weekly_f_sleep[index].iloc[-1, 0].date())
+        for index in range(len(weekly_f_sleep))
+    ],
+    "awake_proportion": [round(val[0], 2) for val in stage_proportions],
+    "light_sleep_proportion": [round(val[1], 2) for val in stage_proportions],
+    "deep_sleep_proportion": [round(val[2], 2) for val in stage_proportions],
+    "rem_sleep_proportion": [round(val[3], 2) for val in stage_proportions],
+    "start_min_time": [val[0] for val in st_start_time],
+    "start_max_time": [val[1] for val in st_start_time],
+    "start_avg_time": [val[2] for val in st_start_time],
+    "start_deviation": [val[3] for val in st_start_time],
+    "end_min_time": [val[0] for val in st_end_time],
+    "end_max_time": [val[1] for val in st_end_time],
+    "end_avg_time": [val[2] for val in st_end_time],
+    "end_deviation": [val[3] for val in st_end_time],
+    "time_to_deep_sleep": [val[0] for val in time_to_sound_sleep],
+    "time_to_rem_sleep": [val[1] for val in time_to_sound_sleep],
+    "deep_sleep_start_proportion": [val[0] for val in prop_sound_sleep],
+    "rem_sleep_start_proportion": [val[1] for val in prop_sound_sleep],
+}
+
+final_insights = pd.DataFrame(final_insights)
+
+final_insights["start_deviation"] = final_insights["start_deviation"].apply(
+    lambda value: (
+        datetime.strptime(value, "%H:%M:%S") - datetime(1900, 1, 1)
+    ).total_seconds()
+    / 60.0
+)
+final_insights["end_deviation"] = final_insights["end_deviation"].apply(
+    lambda value: (
+        datetime.strptime(value, "%H:%M:%S") - datetime(1900, 1, 1)
+    ).total_seconds()
+    / 60.0
+)
+
+
+def getTimeComponents(timestamp):
+    timestamp_obj = datetime.strptime(str(timestamp), "%H:%M:%S")
+
+    return (timestamp_obj.hour, timestamp_obj.minute)
+
+
+results = []
+
+for _, row in final_insights.iterrows():
+    scores = np.zeros(8)
+
+    st_hour, st_minutes = getTimeComponents(row["start_avg_time"])
+    et_hour, et_minutes = getTimeComponents(row["end_avg_time"])
+
+    if 18 <= st_hour <= 23:
+        scores[0] = 1
+    else:
+        if st_hour == 0 and st_minutes < 30:
+            scores[0] = 1
+        else:
+            scores[0] = 2
+
+    if 0 <= et_hour <= 7:
+        scores[1] = 1
+    else:
+        scores[1] = 2
+
+    if row["start_deviation"] < 90:
+        scores[2] = scores[0]
+    else:
+        scores[2] = 3
+
+    if row["end_deviation"] < 90:
+        scores[3] = scores[1]
+    else:
+        scores[3] = 3
+
+    if np.ceil(row["time_to_deep_sleep"] / 60) < 30:
+        scores[4] = np.random.choice([scores[0], scores[1]], 1)[0]
+    else:
+        scores[4] = 3
+
+    if np.ceil(row["time_to_rem_sleep"] / 60) < 90:
+        scores[5] = np.random.choice([scores[0], scores[1]], 1)[0]
+    else:
+        scores[5] = 3
+
+    if row["awake_proportion"] < 0.07:
+        scores[6] = 1
+    elif 0.07 <= row["awake_proportion"] <= 0.12:
+        scores[6] = 2
+    else:
+        scores[6] = 3
+
+    if row["rem_sleep_proportion"] < 0.16:
+        scores[7] = 3
+    elif 0.16 <= row["rem_sleep_proportion"] <= 0.18:
+        scores[7] = 2
+    else:
+        scores[7] = 1
+
+    results.append(scores)
+
+labels = []
+
+for row in results:
+    deviation = np.where(row == 3.0)[0]
+    if len(deviation) > 2:
+        labels.append("Dolphin")
+    else:
+        label1 = np.where(row == 1.0)[0]
+        label2 = np.where(row == 2.0)[0]
+
+        if len(label1) > len(label2):
+            labels.append("Bear")
+        else:
+            labels.append("Wolf")
+
+final_insights["labels"] = labels
+
+
+df = final_insights.drop(
+    [
+        "week_start",
+        "week_end",
+        "start_min_time",
+        "start_max_time",
+        "start_avg_time",
+        "end_min_time",
+        "end_max_time",
+        "end_avg_time",
+        "time_to_deep_sleep",
+        "time_to_rem_sleep",
+    ],
+    axis=1,
+)
+
+y = df.loc[:, "labels"]
+X = df.iloc[:, :-1]
+
+X = X.to_numpy()
+y = y.to_numpy()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=6
+)
+
+clf = make_pipeline(StandardScaler(), SVC(gamma="auto"))
+clf.fit(X_train, y_train)
+print(clf.score(X_test, y_test))
+
+end_time = time.time()
+print(end_time - start_time)
